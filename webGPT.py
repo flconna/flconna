70da3ae1f0eaf39be653f07d7a186df4cd588229
flconna <57486472+flconna@users.noreply.github.com>
2023-02-12 20:09:33 +0800

    Update webGPT.py

--- webGPT.py
+++ webGPT.py
@@ -8,7 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 st.title("OpenAI 文本生成 微信：青森创造")
 
 key_info = st.text_input("输入你的关键字信息使用 '、'分隔")
-length = st.slider("选择你需要生成的文章长度", min_value=1, max_value=500, value=100)
+length = int(st.slider("选择你需要生成的文章长度", min_value=1, max_value=500, value=100))
 
 if st.button("生成文章"):
     completions = openai.Completion.create(
