32188e4ed681d79a6f4dc036836454e0a20216cd
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 14:05:13 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,18 +1,20 @@
 import os
 import openai
 import streamlit as st
-from PIL import Image
 
 # Set OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # 设置OpenAI模型
 model_engine = "text-davinci-003"
-# 添加Logo图片
-logo = Image.open("LOGO.png")
-st.image(logo, use_column_width=False, width=200)
+
+# 添加空白区域来调整页面高度
+header = st.empty()
+
+# 在空白区域中添加图片
+header.image("LOGO.png", width=150)
 
 # 添加文本输入框
-user_input = st.text_area("请输入您的问题：", height=200)
+user_input = st.text_area("请输入您的文本：", height=200)
 
 # 添加确认按钮
 if st.button("确认"):
@@ -20,16 +22,15 @@ if st.button("确认"):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=user_input,
-        max_tokens=2048,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
+    
     # 从API响应中获取结果
     result = response.choices[0].text
     
     # 将结果输出到下方的文本框
-    st.markdown("<hr/>", unsafe_allow_html=True)
-    st.write("<h3 style='text-align: center;'>OpenAI API的回答：</h3>", unsafe_allow_html=True)
-    st.markdown("<hr/>", unsafe_allow_html=True)
-    st.write(f"<div style='font-size: 20px;'>{result}</div>", unsafe_allow_html=True)
+    st.write("OpenAI API的回答：")
+    st.write(result)
