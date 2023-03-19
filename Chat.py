a80e7583b5f07d250a42751da328995050e49320
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:28:49 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,46 +1,25 @@
-import streamlit as st
 import openai
-import os
+import streamlit as st
 
-# Set OpenAI API key
+# Set OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# GPT-3 Completion function
-def ask_gpt3(prompt):
-    response = openai.Completion.create(
-      engine="davinci",
-      prompt=prompt,
-      max_tokens=1024,
-      n=1,
-      stop=None,
-      temperature=0.5,
+st.title("简单的问答系统")
+
+# Get user input
+user_input = st.text_input("请输入您的问题：")
+
+if user_input:
+    # Call OpenAI to generate answer
+    completions = openai.Completion.create(
+        engine="davinci",
+        prompt=user_input,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
-
-    return response.choices[0].text.strip()
-
-# Define Streamlit app
-def app():
-    # Page title
-    st.title("问答系统")
-
-    # Text input for user
-    user_input = st.text_input("你的问题:")
-
-    # Initialize AI response
-    ai_response = ""
-
-    # Check if user has entered text
-    if user_input:
-        # Display user question
-        st.text_area("用户:", value=user_input, key='user_input', height=30, background="#f0f0f0")
-
-        # Generate AI response
-        prompt = f"Question: {user_input}\nAnswer:"
-        ai_response = ask_gpt3(prompt)
-
-        # Display AI response
-        st.text_area("机器人:", ai_response, height=len(ai_response.split("\n")), background="#f0f0f0")
-
-# Run Streamlit app
-if __name__ == "__main__":
-    app()
+    message = completions.choices[0].text.strip()
+    
+    # Show AI answer
+    st.text_area("AI回答：", message, height=30, background="#f0f0f0")
