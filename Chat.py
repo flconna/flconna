f4aed7783bbd74edb7a37089b0805a4e7ecccebb
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:35:54 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -4,21 +4,20 @@ import streamlit as st
 
 # Set OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# 创建Streamlit应用程序
+st.title("ChatGPT")
 
-# 创建文本输入框
+# 添加一个文本输入框，以便用户输入问题
 question = st.text_input("请输入您的问题")
 
-# 创建确认按钮
-if st.button("提交问题"):
-    # 使用OpenAI获取答案
-    answer = openai.Completion.create(
-        engine="davinci",
-        prompt=question,
-        temperature=0.5,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        timeout=5,
+# 添加一个“确认”按钮，以便用户提交问题
+if st.button("确认"):
+    # 调用OpenAI API，并传递问题作为“prompt”参数
+    response = openai.Completion.create(
+        engine="davinci-3", prompt=question, max_tokens=1024, n=1, stop=None, temperature=0.7,
     )
-    # 显示答案
-    st.text(answer.choices[0].text)
+    # 获取API返回的答案
+    answer = response.choices[0].text.strip()
+    
+    # 在Streamlit应用程序中添加一个只读文本框，以显示GPT的答案
+    st.text_area("ChatGPT 的回答", value=answer, height=200, max_chars=None, key=None)
