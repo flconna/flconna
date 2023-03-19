4e6a36f97a6e0b0089401e9d7ab42b325b3d4a29
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 12:55:49 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -4,29 +4,27 @@ import streamlit as st
 
 # 认证openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
-import openai
-import streamlit as st
 
-# 定义Streamlit页面的布局和样式
-st.set_page_config(page_title="Chat with OpenAI", page_icon=":speech_balloon:")
-st.title("Chat with OpenAI")
-st.markdown("---")
+# 设置 OpenAI 引擎的名称。
+model_engine = "text-davinci-003"
 
-# 定义一个函数来获取OpenAI的回答
-def get_answer(prompt):
-    # 调用OpenAI API来生成回答
+# 显示聊天机器人的标题。
+st.title("OpenAI 聊天机器人")
+
+# 获取用户输入并向 OpenAI API 发送请求以获取响应。
+user_input = st.text_input("您: ", "")
+if user_input:
     response = openai.Completion.create(
-        engine="davinci", prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5
+        engine=model_engine,
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
-    # 返回回答
-    return response.choices[0].text.strip()
 
-# 在页面上创建一个文本框来获取用户的问题
-question = st.text_input("Ask a question:")
+    # 提取响应中的答案文本。
+    answer = response.choices[0].text.strip()
 
-# 在页面上创建一个确认按钮，当用户点击时获取OpenAI的回答
-if st.button("Submit"):
-    # 获取OpenAI的回答
-    answer = get_answer(question)
-    # 在页面上创建一个灰色背景的文本框来显示OpenAI的回答
-    st.text_area(answer, height=len(answer.split('\n')), background='#f0f0f0')
+    # 将答案显示在文本区域中。
+    st.text_area("机器人:", answer, height=len(answer.split("\n")), background="#f0f0f0")
