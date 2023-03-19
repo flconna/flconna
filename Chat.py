068a7d6bfb577d5ed8de866feacc34293de70b66
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:24:15 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,22 +1,19 @@
-import os
-import openai
 import streamlit as st
-import time
+import openai
+import os
 
-# Set OpenAI API endpoint and engine
-openai.api_endpoint = "https://api.openai.com"
+# Set OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
-model_engine = "text-davinci-003"
 
 # GPT-3 Completion function
 def ask_gpt3(prompt):
     response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+      engine="davinci",
+      prompt=prompt,
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.5,
     )
 
     return response.choices[0].text.strip()
@@ -24,10 +21,10 @@ def ask_gpt3(prompt):
 # Define Streamlit app
 def app():
     # Page title
-    st.title("中文问答系统")
+    st.title("问答系统")
 
     # Text input for user
-    user_input = st.text_input("请输入你的问题:")
+    user_input = st.text_input("你的问题:")
 
     # Initialize AI response
     ai_response = ""
@@ -37,15 +34,8 @@ def app():
         # Display user question
         st.text_area("用户:", user_input, height=30, background="#f0f0f0")
 
-        # Show loading message
-        progress_bar = st.progress(0)
-        with st.spinner('机器人正在思考中，请稍等...'):
-            for i in range(100):
-                time.sleep(0.02)
-                progress_bar.progress(i + 1)
-
         # Generate AI response
-        prompt = f"问题: {user_input}\n答案:"
+        prompt = f"Question: {user_input}\nAnswer:"
         ai_response = ask_gpt3(prompt)
 
         # Display AI response
