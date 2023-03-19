2af1542739f4ee6ebcc94dd77880956588a0de17
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:30:28 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -5,22 +5,24 @@ import streamlit as st
 # Set OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-st.title("简单的问答系统")
+st.title("问答系统")
 
 # Get user input
-user_input = st.text_input("请输入您的问题：")
+user_input = st.text_input("输入你的问题：")
 
+# Call OpenAI API to get answer
 if user_input:
-    # Call OpenAI to generate answer
-    completions = openai.Completion.create(
+    response = openai.Completion.create(
         engine="davinci",
         prompt=user_input,
-        max_tokens=100,
+        max_tokens=10,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    message = completions.choices[0].text.strip()
-    
-    # Show AI answer
-    st.text_area("AI回答：", message, height=30, background="#f0f0f0")
+    message = response.choices[0].text.strip()
+else:
+    message = ""
+
+# Display AI response
+st.text_area("AI回答：", message, height=30, background="#f0f0f0")
