f031fa96a90944075ec0047bc2eac7ae9a539d64
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:06:14 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,34 +1,53 @@
-import os
-import openai
 import streamlit as st
-import json
-# 认证openai
+import openai
+import time
+
+# Set OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Define a function to ask the OpenAI GPT-3 API a question
+# GPT-3 Completion function
 def ask_gpt3(prompt):
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=2048,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+      engine="davinci",
+      prompt=prompt,
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.5,
     )
-    answer = response.choices[0].text
-    return answer.strip()
 
-# Define the main Streamlit app
+    return response.choices[0].text.strip()
+
+# Define Streamlit app
 def app():
-    st.set_page_config(page_title="Chat with GPT-3", page_icon=":robot_face:")
-    st.title("Chat with GPT-3")
-    user_input = st.text_input("You:", "")
-    if user_input:
-        prompt = f"User: {user_input}\nAI:"
-        answer = ask_gpt3(prompt)
-        st.text_area("机器人:", answer, height=len(answer.split("\n")), background_color="#f0f0f0")
+    # Page title
+    st.title("问答系统")
 
-# Run the app
+    # Text input for user
+    user_input = st.text_input("你的问题:")
+
+    # Initialize AI response
+    ai_response = ""
+
+    # Check if user has entered text
+    if user_input:
+        # Display user question
+        st.text_area("用户:", user_input, height=30, background="#f0f0f0")
+
+        # Show loading message
+        progress_bar = st.progress(0)
+        with st.spinner('机器人正在思考中，请稍等...'):
+            for i in range(100):
+                time.sleep(0.02)
+                progress_bar.progress(i + 1)
+
+        # Generate AI response
+        prompt = f"Question: {user_input}\nAnswer:"
+        ai_response = ask_gpt3(prompt)
+
+        # Display AI response
+        st.text_area("机器人:", ai_response, height=len(ai_response.split("\n")), background="#f0f0f0")
+
+# Run Streamlit app
 if __name__ == "__main__":
     app()
