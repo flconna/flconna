7ceaf1a725c1dde56501ec09005afec0ebdec316
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 12:59:23 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -5,26 +5,37 @@ import streamlit as st
 # 认证openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# 设置 OpenAI 引擎的名称。
-model_engine = "text-davinci-003"
+# 获取 OpenAI 官方的文本生成模型 ID，这里使用了 GPT-3 的英文模型
+model_engine = "text-davinci-002"
 
-# 显示聊天机器人的标题。
-st.title("OpenAI 聊天机器人")
-
-# 获取用户输入并向 OpenAI API 发送请求以获取响应。
-user_input = st.text_input("您: ", "")
-if user_input:
+# 获取答案函数
+def get_answer(prompt):
     response = openai.Completion.create(
         engine=model_engine,
-        prompt=user_input,
+        prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.7,
     )
+    answer = response.choices[0].text
+    return answer.strip()
 
-    # 提取响应中的答案文本。
-    answer = response.choices[0].text.strip()
+# 定义 Streamlit 的应用程序
+def app():
+    # 在 Streamlit 中创建标题
+    st.title("Chatbot")
 
-    # 将答案显示在文本区域中。
-    st.text_area("机器人:", str(answer), height=len(answer.split("\n")), background="#f0f0f0")
+    # 在 Streamlit 中创建文本输入框，用于用户输入问题
+    user_input = st.text_input("你的问题：")
+
+    # 当用户按下回车键时，将用户输入的问题发送给 OpenAI 的文本生成模型，并获取答案
+    if st.button("发送"):
+        prompt = f"Q: {user_input}\nA:"
+        answer = get_answer(prompt)
+
+        # 将答案显示在 Streamlit 中的文本框中
+        st.text_area("机器人:", str(answer), height=len(answer.split("\n")), background="#f0f0f0")
+
+if __name__ == "__main__":
+    app()
