84af7c1fff1e99c868e490ae3f99fe62af37b7a8
flconna <57486472+flconna@users.noreply.github.com>
2023-03-19 13:59:54 +0800

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,47 +1,37 @@
 import os
 import openai
 import streamlit as st
+from PIL import Image
 
 # Set OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # 设置OpenAI模型
 model_engine = "text-davinci-003"
 
-# 设置网页标题和宽度
-st.set_page_config(page_title="文本模型问答", page_icon=":pencil2:", layout="wide")
-
-# 添加页面标题
-st.title("OpenAI文本模型问答")
+# 添加Logo图片
+logo = Image.open("LOGO.png")
+st.image(logo, use_column_width=False, width=100)
 
 # 添加文本输入框
-with st.form(key="text_input_form"):
-    st.write("请输入您的文本：")
-    user_input = st.text_area("", height=200, key="user_input")
-    # 添加确认按钮
-    submitted = st.form_submit_button("确认")
+user_input = st.text_area("请输入您的文本：", height=200)
 
-# 处理确认按钮提交的事件
-if submitted:
-    if user_input:
-        # 发送请求到OpenAI API
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=user_input,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-
-        # 从API响应中获取结果
-        result = response.choices[0].text.strip()
-        
-        # 处理回答的格式
-        result = result.replace("\n", "<br>")
-        result = "<div style='text-align: left; font-size: 18px; line-height: 1.5;'>" + result + "</div>"
-
-        # 将结果输出到下方的文本框
-        st.write("OpenAI API的回答：", unsafe_allow_html=True)
-        st.write(result, unsafe_allow_html=True)
-    else:
-        st.warning("请输入您的问题！")
+# 添加确认按钮
+if st.button("确认"):
+    # 发送请求到OpenAI API
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    # 从API响应中获取结果
+    result = response.choices[0].text
+    
+    # 将结果输出到下方的文本框
+    st.markdown("<hr/>", unsafe_allow_html=True)
+    st.write("<h3 style='text-align: center;'>OpenAI API的回答：</h3>", unsafe_allow_html=True)
+    st.markdown("<hr/>", unsafe_allow_html=True)
+    st.write(f"<div style='font-size: 20px;'>{result}</div>", unsafe_allow_html=True)
